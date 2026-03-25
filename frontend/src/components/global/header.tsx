ef04c142db2c27bc54e54099609768a284f79a02
Aidan <aidanbh16@gmail.com>
2026-03-25 09:24:06 -0400

    Header and Logo

--- frontend/src/components/global/header.tsx
+++ frontend/src/components/global/header.tsx
@@ -1,12 +1,15 @@
 
+import Image from "next/image"
+
+import Logo from "../../../public/logo/Lucrum Logo.png"
 
 export default function Header(){
     return(
-        <header className="w-full h-10 bg-white flex">
-            <div className="w-1/2">
-                Logo
+        <header className="w-full h-10 px-5 bg-white flex justify-center">
+            <div className="flex items-center gap-x-2">
+                <Image src={Logo} alt="Logo" className="h-9/10 w-15"/>
+                <h1 className="text-amber-400 text-2xl font-semibold text-shadow-sm">Lucrum</h1>
             </div>
-            <div className="w-1/2"></div>
         </header>
     )
 }
