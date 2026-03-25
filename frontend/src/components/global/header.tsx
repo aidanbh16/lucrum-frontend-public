94b75dc8e26899716b4802f417cc084410636a87
Aidan <aidanbh16@gmail.com>
2026-03-25 13:59:23 -0400

    Removed unnecessary responsiveness to header

--- frontend/src/components/global/header.tsx
+++ frontend/src/components/global/header.tsx
@@ -5,7 +5,7 @@ import Logo from "../../../public/logo/Lucrum Logo.png"
 
 export default function Header(){
     return(
-        <header className="max-md:hidden w-full h-10 px-5 bg-white flex justify-center fixed shadow-lg">
+        <header className="w-full h-10 px-5 bg-white flex justify-center fixed shadow-lg">
             <div className="flex items-center gap-x-2 select-none">
                 <Image src={Logo} alt="Logo" className="h-9/10 w-15"/>
                 <h1 className="text-amber-400 text-3xl font-semibold text-shadow-sm">Lucrum</h1>
