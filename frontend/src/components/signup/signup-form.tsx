7c555cf4e5aef46e0d9f62edba9dda480989726c
Aidan <aidanbh16@gmail.com>
2026-03-26 11:10:30 -0400

    Pushing everything for project mates

--- frontend/src/components/signup/signup-form.tsx
+++ frontend/src/components/signup/signup-form.tsx
@@ -4,10 +4,10 @@ export default function SignupForm(){
     return(
         <form className="w-full flex flex-col items-center gap-y-3">
             <fieldset className="w-2/3 flex flex-col justify-center gap-y-3">
-                <input type="text" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Username"/>
-                <input type="email" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Email"/>
-                <input type="password" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Password"/>
-                <input type="password" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Confirm Password"/>
+                <input type="text" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Username" required/>
+                <input type="email" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Email" required/>
+                <input type="password" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Password" required/>
+                <input type="password" className="px-2 py-1 text-black border outline-none rounded-md" placeholder="Confirm Password" required/>
             </fieldset>
             <fieldset className="w-full flex flex-col items-center gap-y-2">
                 <button type="submit" className="w-1/3 h-8 bg-amber-400 border-amber-400 border-2 text-white font-semibold rounded-md cursor-pointer transition-all duration-200 hover:bg-white hover:text-amber-400">Signup</button>
